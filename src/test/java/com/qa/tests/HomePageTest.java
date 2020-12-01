0bb3172e801ddd6955622551a164c9877b19e676
Git-Testi <yashinith16@gmail.com>
2020-11-30 23:35:06 -0700

    initial commit

---
+++ src/test/java/com/qa/tests/HomePageTest.java
@@ -0,0 +1,10 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+@Test
+public void homePage(){
+	System.out.println("HomePage test");
+}
+}
---
+++ src/test/java/com/qa/tests/SearchPageTest.java
@@ -0,0 +1,7 @@
+package com.qa.tests;
+
+public class SearchPageTest {
+public void searchPage(){
+	System.out.println();
+}
+}
